9b1fc78609e1c8261c7d5de16c2116bd79d55314
unknown <aimanajihadi@gmail.com>
2025-11-20 13:18:34 +0700

    profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -10,35 +10,82 @@ function Profile() {
 
                 </div>
 
-                <div className={style.title}>
-                    <div className={style.name}></div>
-                    <div className={style.alamat}></div>
-                    <div className={style.id}></div>
-                </div>
+                <div className={style.name}>
+                        <h1>Name:</h1>
+                        <h1>Alamat:</h1>
+                        <h1>Id: </h1>
+                    </div>
 
-                <div className={style.bulat}>
-                    <img src='/image/malam-mode.png' alt="night mode" />
-                </div>
+
+                    <div className={style.bulat}>
+                        <img src='image/malam-mode.png' />
+                    </div>
+
+
 
             </div>
 
             <div className={style.bawah}>
 
-                <div className={style.level}>
-                    <div className={style.approve}></div>
+                <div className={style.tutorial}>
+                    <div className={style.log}>
+                        <img src='image/react.png' />
+                    </div>
+                    <div className={style.dess}>
+                        <h1>DESKRIPSI</h1>
+                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                             when an unknown printer took a galley of type and scrambled it to make a type 
+                             specimen book. It has survived not only five centuries, but also the leap into 
+                             electronic typesetting, remaining essentially unchanged. </h4>
+                    </div>
+                    <div className={style.persen}>
+                        <h1>0%</h1>
+                    </div>
                 </div>
 
-                <div className={style.level}>
-                    <div className={style.approve}></div>
+
+                <div className={style.tutorial}>
+                    <div className={style.log}>
+                        <img src='image/html.jpg' />
+                    </div>
+                    <div className={style.dess}>
+                        <h1>DESKRIPSI</h1>
+                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                             when an unknown printer took a galley of type and scrambled it to make a type 
+                             specimen book. It has survived not only five centuries, but also the leap into 
+                             electronic typesetting, remaining essentially unchanged. </h4>
+                    </div>
+                    <div className={style.persen}>
+                        <h1>0%</h1>
+                    </div>
                 </div>
 
-                <div className={style.level}>
-                    <div className={style.approve}></div>
+
+
+                <div className={style.tutorial}>
+                    <div className={style.log}>
+                        <img src='image/css.jpg' />
+                    </div>
+                    <div className={style.dess}>
+                        <h1>DESKRIPSI</h1>
+                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                             when an unknown printer took a galley of type and scrambled it to make a type 
+                             specimen book. It has survived not only five centuries, but also the leap into 
+                             electronic typesetting, remaining essentially unchanged. </h4>
+                    </div>
+                    <div className={style.persen}>
+                        <h1>0%</h1>
+                    </div>
                 </div>
 
-                <div className={style.level}>
-                    <div className={style.approve}></div>
-                </div>
+
+
+
+
+
 
             </div>
         </div>
